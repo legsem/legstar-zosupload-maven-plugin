f5a7f9aa81fcdae95fbbd081f212a8cb6a778e91
fady.moussallam <fady.moussallam@a12fe416-eba9-11de-88d6-afc75ad17f39>
2011-03-09 08:00:26 +0000

    Fixing Issue 2: JCL error number not detected

--- trunk/src/test/java/com/legstar/zosjes/ZosUploadMojoTest.java
+++ trunk/src/test/java/com/legstar/zosjes/ZosUploadMojoTest.java
@@ -17,119 +17,147 @@ import java.util.LinkedList;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
-import com.legstar.zosjes.HostSettings;
-import com.legstar.zosjes.ZosUploadMojo;
-
 /**
- * Unit test the plugin. 
- *
+ * Unit test the plugin.
+ * 
  */
 public class ZosUploadMojoTest extends AbstractMojoTestCase {
 
-	/** Parameters needed to submit a job to z/OS. */
-	private HostSettings _hostSettings = new HostSettings();
-	
-	/** Setup the plugin environment. */
-	protected void setUp() throws Exception {
+    /** Parameters needed to submit a job to z/OS. */
+    private HostSettings _hostSettings = new HostSettings();
+
+    /** Setup the plugin environment. */
+    protected void setUp() throws Exception {
         super.setUp();
     }
 
     /**
      * tests the proper discovery and configuration of the mojo
-     *
+     * 
      * @throws Exception
      */
- 	public void testConfiguration() throws Exception {
+    public void testConfiguration() throws Exception {
 
         ZosUploadMojo mojo = new ZosUploadMojo();
-        configureMojo (mojo, "zosjes-maven-plugin", getTestPom() );
-        
+        configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
+
         assertEquals(_hostSettings.getHostName(),
-        		(String)getVariableValueFromObject(mojo, "hostName") );
+                (String) getVariableValueFromObject(mojo, "hostName"));
         assertEquals(_hostSettings.getHostUserId(),
-        		(String)getVariableValueFromObject(mojo, "hostUserId") );
+                (String) getVariableValueFromObject(mojo, "hostUserId"));
         assertEquals(_hostSettings.getHostPassword(),
-        		(String)getVariableValueFromObject(mojo, "hostPassword") );
+                (String) getVariableValueFromObject(mojo, "hostPassword"));
         assertEquals("zos",
-        	((File) getVariableValueFromObject(mojo, "inputFolder")).getName());
+                ((File) getVariableValueFromObject(mojo, "inputFolder"))
+                        .getName());
         assertEquals("P390.LIB",
-            	(String) getVariableValueFromObject(mojo, "remoteFilesPrefix"));
+                (String) getVariableValueFromObject(mojo, "remoteFilesPrefix"));
     }
-    
+
     /**
      * Test that we are able to submit actual jobs.
+     * 
      * @throws Exception if test fails
      */
     public void testExecution() throws Exception {
         ZosUploadMojo mojo = new ZosUploadMojo();
-        configureMojo (mojo, "zosjes-maven-plugin", getTestPom() );
+        configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
         mojo.execute();
     }
-    
+
     /**
      * Test that we are able to submit actual jobs when explicitly selected.
+     * 
      * @throws Exception if test fails
      */
     public void testExecutionFromList() throws Exception {
         ZosUploadMojo mojo = new ZosUploadMojo();
-        configureMojo (mojo, "zosjes-maven-plugin", getTestPom() );
+        configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
         mojo.jclFileNames = new LinkedList < String >();
         mojo.jclFileNames.add("LISTCAT");
         mojo.execute();
     }
-    
+
     /**
      * Test what happens when a job fails.
+     * 
      * @throws Exception if test fails
      */
     public void testFailedExecution() {
         try {
-			ZosUploadMojo mojo = new ZosUploadMojo();
-			configureMojo (mojo, "zosjes-maven-plugin", getTestPom() );
-			mojo.remoteFilesPrefix = "P390.LIB";
-			mojo.inputFolder = new File("src/test/resources/zosfail");
-			mojo.execute();
-		} catch (Exception e) {
-			assertEquals("Job submitted to z/OS failed.  Highest condition code: 12",
-					e.getMessage());
-		}
+            ZosUploadMojo mojo = new ZosUploadMojo();
+            configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
+            mojo.remoteFilesPrefix = "P390.LIB";
+            mojo.inputFolder = new File("src/test/resources/zosfail");
+            mojo.execute();
+            fail();
+        } catch (Exception e) {
+            assertEquals(
+                    "Job submitted to z/OS failed.  Highest condition code: 12",
+                    e.getMessage());
+        }
     }
 
     /**
      * Test what happens when a job abends.
+     * 
      * @throws Exception if test fails
      */
     public void testAbendedExecution() {
         try {
             ZosUploadMojo mojo = new ZosUploadMojo();
-            configureMojo (mojo, "zosjes-maven-plugin", getTestPom() );
+            configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
             mojo.remoteFilesPrefix = "P390.LIB";
             mojo.inputFolder = new File("src/test/resources/zosabend");
             mojo.execute();
+            fail();
         } catch (Exception e) {
-            assertEquals("Job submitted to z/OS failed.  Highest condition code: 806",
+            assertEquals(
+                    "Job submitted to z/OS failed.  Highest condition code: 806",
+                    e.getMessage());
+        }
+    }
+
+    /**
+     * Test what happens when a job fails with a JCL error.
+     * 
+     * @throws Exception if test fails
+     */
+    public void testJclErrorExecution() {
+        try {
+            ZosUploadMojo mojo = new ZosUploadMojo();
+            configureMojo(mojo, "zosjes-maven-plugin", getTestPom());
+            mojo.remoteFilesPrefix = "P390.LIB";
+            mojo.inputFolder = new File("src/test/resources/zosjclerror");
+            mojo.execute();
+            fail();
+        } catch (Exception e) {
+            assertEquals(
+                    "Job submitted to z/OS failed.  Highest condition code: 9",
                     e.getMessage());
         }
     }
 
     /**
      * Return a test pom where variables have been replaced by actual values.
+     * 
      * @throws IOException if test pom cannot be read
      */
     protected File getTestPom() throws IOException {
-        File testPom = new File( getBasedir(),
-		"target/test-classes/unit/basic-test-plugin-config.xml" );
+        File testPom = new File(getBasedir(),
+                "target/test-classes/unit/basic-test-plugin-config.xml");
         String testPomStr = FileUtils.readFileToString(testPom);
-        testPomStr = testPomStr.replace("${" + HostSettings.HOST_NAME_TAG + "}",
-        		_hostSettings.getHostName());
-        testPomStr = testPomStr.replace("${" + HostSettings.HOST_USER_ID_TAG + "}",
-        		_hostSettings.getHostUserId());
-        testPomStr = testPomStr.replace("${" + HostSettings.HOST_PASSWORD_TAG + "}",
-        		_hostSettings.getHostPassword());
+        testPomStr = testPomStr.replace(
+                "${" + HostSettings.HOST_NAME_TAG + "}",
+                _hostSettings.getHostName());
+        testPomStr = testPomStr.replace("${" + HostSettings.HOST_USER_ID_TAG
+                + "}", _hostSettings.getHostUserId());
+        testPomStr = testPomStr.replace("${" + HostSettings.HOST_PASSWORD_TAG
+                + "}", _hostSettings.getHostPassword());
         File tempPom = File.createTempFile("legstar", "test.pom.xml");
         tempPom.deleteOnExit();
         FileUtils.writeStringToFile(tempPom, testPomStr);
         return tempPom;
-    	
+
     }
 }
